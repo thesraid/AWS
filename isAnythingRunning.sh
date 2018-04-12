860fcac4efad4e5fe4f46788cee466f7561fe6b4
Ubuntu <ubuntu@ip-172-31-28-25.eu-west-1.compute.internal>
2018-04-12 08:02:44 +0000

    update

---
+++ isAnythingRunning.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+for accounts in {01..07}
+do
+   s="AVStudent$accounts"
+   p="Profile"
+   profile=$s$p
+   echo $profile
+   echo "Stacks : "
+   aws cloudformation describe-stacks --query Stacks[*].StackName --output text --profile $profile
+   echo "Instances : "
+   aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text --profile $profile
+   printf "\n"
+done
+
+printf "cliaccount\n"
+printf "Stacks : "
+aws cloudformation describe-stacks --query Stacks[*].StackName --output text --profile cliaccount
+echo "Instances : "
+aws ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`Name`].Value]' --output text --profile cliaccount
+printf "\n"
