e670c67665acdd12dae82cf01d574613867f1daf
Ubuntu <thesraid@gmail.com>
2019-02-07 11:34:34 +0000

    Added line to remove termination protection

--- intermediate/removeTermProtect.sh
+++ intermediate/removeTermProtect.sh
@@ -5,7 +5,7 @@ i=101
 while [ $i -le 114 ]
 do
    aws configure set region us-east-1 --profile AVStudent$i
-   ami=($(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --filters Name=tag:Name,Values=Sensor --output=text --profile AVStudent$i))
+   ami=($(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output=text --profile AVStudent$i))
    echo "AVStudent$i : $ami"
    aws ec2 modify-instance-attribute --no-disable-api-termination --instance-id $ami --profile AVStudent$i
    i=$[$i+1]
