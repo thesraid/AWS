840d9215493acf46d623e51074f41b0991afe0ff
Ubuntu <thesraid@gmail.com>
2019-11-29 10:54:22 +0000

    Moving to CodeCommit

---
+++ share-ami.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# joriordan
+
+# Prints the script usage information
+function usage
+{
+    echo "share-ami [-h] 
+          --subs [-s] LIST_OF_SUBORGS_TEXTFILE
+          --region [-r] REGION
+          --ami [-a] ami-1234567890abcdef | all
+         "
+}
+
+# Read the input from the command.
+while [ "$1" != "" ]; do
+    case $1 in
+        -s | --subs )           shift
+                                subs=$1
+                                ;;
+        -a | --ami )            shift
+                                ami=$1
+                                ;;
+        -r | --region )         shift
+                                region=$1
+                                ;;
+        -h | --help )           usage
+                                exit
+                                ;;
+    esac
+    shift
+done
+
+echo " "
+echo "This will update $ami in  $region to only allow access from the SubOrgs specified. Hit Ctrl+C to cancel"
+echo "5"
+sleep 1
+echo "4"
+sleep 1
+echo "3"
+sleep 1
+echo "2"
+sleep 1
+echo "1"
+sleep 1
+
+if [ "$ami" = "all" ]
+then
+IMAGES=($(aws ec2 describe-images --owners self --query 'Images[*].[ImageId]' --output text --region $region))
+else
+IMAGES=($ami)
+fi
+
+for image in "${IMAGES[@]}"
+do
+   printf "Modifying "
+   aws ec2 describe-images --owners self --query 'Images[*].[Name,ImageId]' --output text --region $region | grep $image
+   aws ec2 modify-image-attribute --image-id $image --launch-permission "{\"Remove\": [{\"Group\":\"all\"}]}" --region $region
+
+   SUBS=($(cat $subs))
+   for sub in "${SUBS[@]}"
+   do
+      printf "."
+      aws ec2 modify-image-attribute --image-id $image --launch-permission "Add=[{UserId=$sub}]" --region $region
+   done
+
+   echo " "
+done
